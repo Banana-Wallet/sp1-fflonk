ec9bac7feb98e0ed9404f529beafbbbc705d9887
nlok5923 <nlokhande5923@gmail.com>
2024-04-15 14:58:08 +0530

    fixes

--- program/src/main.rs
+++ program/src/main.rs
@@ -21,31 +21,31 @@ pub fn main() {
     // NOTE: values of n larger than 186 will overflow the u128 type,
     // resulting in output that doesn't match fibonacci sequence.
     // However, the resulting proof will still be valid!
-    println!("cycle-tracker-start: setup");
+    // println!("cycle-tracker-start: setup");
 
-    let n = sp1_zkvm::io::read::<u32>();
-    let mut a: u32 = 0;
-    let mut b: u32 = 1;
-    let mut sum: u32;
-    for _ in 1..n {
-        sum = a + b;
-        a = b;
-        b = sum;
-    }
-    println!("cycle-tracker-end: setup");
+    // let n = sp1_zkvm::io::read::<u32>();
+    // let mut a: u32 = 0;
+    // let mut b: u32 = 1;
+    // let mut sum: u32;
+    // for _ in 1..n {
+    //     sum = a + b;
+    //     a = b;
+    //     b = sum;
+    // }
+    // println!("cycle-tracker-end: setup");
 
-    println!("cycle-tracker-start: main-body");
+    println!("cycle-tracker-start: zksync-verification");
     verify();
 
-    println!("cycle-tracker-end: main-body");
+    println!("cycle-tracker-end: zksync-verification");
 
     // run_main_fflonk();
 
 
    
-    sp1_zkvm::io::commit(&n);
-    sp1_zkvm::io::commit(&a);
-    sp1_zkvm::io::commit(&b);
+    // sp1_zkvm::io::commit(&n);
+    // sp1_zkvm::io::commit(&a);
+    // sp1_zkvm::io::commit(&b);
 }
 
 
